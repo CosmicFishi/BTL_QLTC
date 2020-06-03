d97d1dafd685ba64d1cde1e1006636b5005f15d8
Ngô Văn Hậu <43901976+kaneki-sama@users.noreply.github.com>
2020-06-03 10:09:23 +0700

    ms them store

---
+++ mysql/quanlytieccuoi_hoa_don_thuc_uong0.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quanlytieccuoi
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hoa_don_thuc_uong`
+--
+
+DROP TABLE IF EXISTS `hoa_don_thuc_uong`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hoa_don_thuc_uong` (
+  `MaHD` int NOT NULL,
+  `MaThucUong` int NOT NULL,
+  `GiaDv` int NOT NULL,
+  `SoLuong` int NOT NULL,
+  PRIMARY KEY (`MaHD`,`MaThucUong`),
+  UNIQUE KEY `MaHD` (`MaHD`),
+  KEY `fk_hoadonthucuong_thucuong_idx` (`MaThucUong`),
+  CONSTRAINT `fk_hoadonthucuong_hoadon` FOREIGN KEY (`MaHD`) REFERENCES `hoa_don` (`MaHoaDon`),
+  CONSTRAINT `fk_hoadonthucuong_thucuong` FOREIGN KEY (`MaThucUong`) REFERENCES `thuc_uong` (`MaThucUong`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hoa_don_thuc_uong`
+--
+
+LOCK TABLES `hoa_don_thuc_uong` WRITE;
+/*!40000 ALTER TABLE `hoa_don_thuc_uong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hoa_don_thuc_uong` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-03 10:08:56
